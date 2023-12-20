5da0a0c48de449595e040e3deb46c5f377fbd161
Stephen Zhou <hi@hyoban.cc>
2023-12-20 17:47:33 +0800

    fix: style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { AppearanceSwitch } from "~/components/appearance-switch"
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -18,47 +17,51 @@ export default async function Home() {
   const rows = psOut.map((p) => Object.values(p))
 
   return (
-    <div className="h-full flex flex-col max-w-4xl mx-auto gap-4 justify-center items-center">
-      <code className="text-2xl">docker ps -a</code>
-      <Table>
-        <TableCaption>
-          {`Updated at ${format(timestamp, "yyyy-MM-dd HH:mm:ss")}`}
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            {usefulPsRowInfo.map((h) => (
-              <TableHead key={h}>{h}</TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {rows.map((row, rowIndex) => (
-            <TableRow key={row[0]}>
-              {usefulPsRowInfo.map((key) => {
-                const cell = psOut[rowIndex]![key]!
-                return (
-                  <TableCell key={cell} title={cell} className="space-y-2">
-                    {
-                      // eslint-disable-next-line @eslint-react/jsx/no-complicated-conditional-rendering
-                      key === "ID"
-                        ? cell.slice(0, 6) + "..."
-                        : key === "CreatedAt"
-                          ? formatRelative(new Date(cell), new Date())
-                          : key === "Ports"
-                            ? cell
-                                .split(", ")
-                                .filter((port) => !port.startsWith(":::"))
-                                .map((port) => <div key={port}>{port}</div>)
-                            : cell
-                    }
-                  </TableCell>
-                )
-              })}
+    <div className="h-full p-10">
+      <div className="flex flex-col max-w-4xl mx-auto justify-center items-center">
+        <code className="text-2xl my-4">docker ps -a</code>
+        <p className="text-sm opacity-60 my-2">
+          Hi! I'm running on port <code className="font-bold">3000</code>.{" "}
+          {`Updated at ${format(timestamp, "yyyy-MM-dd HH:mm:ss")}.`}
+        </p>
+
+        <AppearanceSwitch className="my-5" />
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {usefulPsRowInfo.map((h) => (
+                <TableHead key={h}>{h}</TableHead>
+              ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <AppearanceSwitch />
+          </TableHeader>
+          <TableBody>
+            {rows.map((row, rowIndex) => (
+              <TableRow key={row[0]}>
+                {usefulPsRowInfo.map((key) => {
+                  const cell = psOut[rowIndex]![key]!
+                  return (
+                    <TableCell key={cell} title={cell} className="space-y-2">
+                      {
+                        // eslint-disable-next-line @eslint-react/jsx/no-complicated-conditional-rendering
+                        key === "ID"
+                          ? cell.slice(0, 6) + "..."
+                          : key === "CreatedAt"
+                            ? formatRelative(new Date(cell), new Date())
+                            : key === "Ports"
+                              ? cell
+                                  .split(", ")
+                                  .filter((port) => !port.startsWith(":::"))
+                                  .map((port) => <div key={port}>{port}</div>)
+                              : cell
+                      }
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
